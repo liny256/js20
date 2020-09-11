bfa900021e1d203cdd9134962e295d7394932b62
林怡文 <linyiwen@linyiwendembp.home>
2020-09-11 12:41:20 -0400

    initial commit

--- 1-index/script.js
+++ 1-index/script.js
@@ -1,11 +1,37 @@
 var xmlhttp = new XMLHttpRequest();
-var url = "data-jason.txt";
+var url = "data-json.txt";
 var data= [];
 xmlhttp.onreadystatechange = function(){
   if(this.readyState == 4 && this.status ==200){
     data = JSON.parse(this.responseText);
     console.log(data);
+    buildIndex(data);
   }
 };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
+
+//build functions
+function buildIndex(data){
+  for (i=0; i<data.length; i++){
+    var item= document.createElement("li");
+    item.classList.add("items");
+    item.classList.add(data[i].num);
+    item.classList.add(data[i].ProjectName);
+
+    var elem= document.createElement("a");
+    elem.innerText= data[i].ProjectName;
+    // elem.href
+
+    var colorBlock= document.createElement("div");
+    item.appendChild(colorBlockL);
+    if(i/2== 1){
+      var colorBlockL= document.createElement("div");
+      item.appendChild(colorBlockL);
+      document.querySelector('.left').appendChild()
+    }
+
+    item.appendChild(elem);
+    document.querySelector('.result').appendChild(item);
+  }
+}
